c97e0fefcc232c1a88948250b688b4663abb3616
FangerZero <FangerZero@hotmail.com>
2020-02-26 17:58:10 -0600

    Veterinary Spelled correctly

--- src/components/adopt/VetInfo.tsx
+++ src/components/adopt/VetInfo.tsx
@@ -8,29 +8,29 @@ const VetInfo = (props: any) => {
     return (
         <IonItemGroup>
             <IonItemDivider color="primary" onClick={() => setShow(!show)}>
-                <IonLabel>{show ? <IonIcon icon={removeCircleOutline} /> : <IonIcon icon={addCircleOutline} />} Vetinary Information</IonLabel>
+                <IonLabel>{show ? <IonIcon icon={removeCircleOutline} /> : <IonIcon icon={addCircleOutline} />} Veterinary Information</IonLabel>
             </IonItemDivider>
             { show &&
                 <IonItem lines="none">
-                    <IonLabel position="floating">Vetinary Name</IonLabel>
+                    <IonLabel position="floating">Veterinary Name</IonLabel>
                     <IonInput type="text" name="vetName" required onIonBlur={e => props.updateForm(e, 'vetName') }></IonInput>
                 </IonItem>
             }
             { show &&  
                 <IonItem lines="none">
-                    <IonLabel position="floating">Vetinary Phone</IonLabel>
+                    <IonLabel position="floating">Veterinary Phone</IonLabel>
                     <IonInput type="tel" name="vetPhone" required onIonBlur={e => props.updateForm(e, 'vetPhone') }></IonInput>
                 </IonItem>
             }
             { show &&  
                 <IonItem lines="none">
-                    <IonLabel position="floating">Vetinary Clinic</IonLabel>
+                    <IonLabel position="floating">Veterinary Clinic</IonLabel>
                     <IonInput type="text" name="vetClinic" required onIonBlur={e => props.updateForm(e, 'vetClinic') }></IonInput>
                 </IonItem>
             }
             { show &&  
                 <IonItem lines="none">
-                    <IonLabel position="floating">Vetinary Address</IonLabel>
+                    <IonLabel position="floating">Veterinary Address</IonLabel>
                     <IonInput type="text" name="vetAddress" required onIonBlur={e => props.updateForm(e, 'vetAddress') }></IonInput>
                 </IonItem>
             }
