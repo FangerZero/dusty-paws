6368e7a6abe9c22be7cd8dd2064fc8e6daa54008
FangerZero <FangerZero@hotmail.com>
2020-02-29 08:34:24 -0600

    April's About me portion completed

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -28,7 +28,7 @@ const About: React.FC = () => {
             </IonCol>
             <IonCol offsetLg="0" sizeLg="5" offsetXs="0" sizeXs="12">
               <h1>April McDowell - Adoption Coordinator/Foster</h1>
-              <p>I have a great peryeneese named Snowy. </p>
+              <p>Everyone asks how I decided to help run a rescue? Well I will tell you the truth it wasn't easy. It starts with Vash, the Doberman, he's so old you have to dust him off when you walk by.  Knowing his time is close, we didn't want our pyr Snowy Girl to be on guard alone so we decided we would foster until the right dog came along. The problem was that there were plenty of right dogs but no right rescues. No one wanted me. My yard wasn't fenced and despite all my resources and experience “no fence no dog”. On the day I chose to give up on being a foster for Pyrenees, Facebook said “you may be interested in this site” and so I clicked on it. God blessed the broken road that led me straight to the Safe Haven and the rest is history. I started out wanting to foster, and now I get to be so much more, new friends, new adventures, God knew exactly where to send me. </p>
             </IonCol>
           </IonRow>
         </IonGrid>
